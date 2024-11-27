703b90c641239aaf3c697aaa358a44ecd803c410
Hendra Lijaya <hendralijaya@gmail.com>
2024-11-27 12:05:50 +0700

    Initial Commit

---
+++ CloneBlueBCATests/CloneBlueBCATests.swift
@@ -0,0 +1,17 @@
+//
+//  CloneBlueBCATests.swift
+//  CloneBlueBCATests
+//
+//  Created by hendra on 27/11/24.
+//
+
+import Testing
+@testable import CloneBlueBCA
+
+struct CloneBlueBCATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ CloneBlueBCAUITests/CloneBlueBCAUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  CloneBlueBCAUITestsLaunchTests.swift
+//  CloneBlueBCAUITests
+//
+//  Created by hendra on 27/11/24.
+//
+
+import XCTest
+
+final class CloneBlueBCAUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
